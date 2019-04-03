50a4116047db80f6c8df5dbfa588eb8c1dc03067
yanglin <xueshuiyy@126.com>
2019-04-03 17:31:33 +0800

    创建对象的4种方式 demo提交，newObj包下

---
+++ src/main/java/com/exer/newObj/Book.java
@@ -0,0 +1,44 @@
+package com.exer.newObj;
+
+/**
+ * Created by Administrator on 2019/4/3.
+ */
+public class Book {
+    private String name;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    private String author;
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
---
+++ src/test/java/com/exer/newObj/ObjTest.java
@@ -0,0 +1,42 @@
+package com.exer.newObj;
+
+import com.exer.aop.Student;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Created by Administrator on 2019/4/3.
+ */
+public class ObjTest {
+
+    @Test
+    public void test() throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, CloneNotSupportedException, IOException {
+        // 方式1,直接new
+        /*Book book = new Book("红楼梦", "曹雪芹");*/
+
+        // 方式2的两种方式归根结底都是实现了ConstructorAccessor接口的newInstance,都是构造函数
+
+        // 方式2.1，用class的newinstance
+        Class c1 = Class.forName("com.exer.newObj.Book");
+        Constructor<Book> bookConstructor  = (Constructor<Book>) c1.getConstructor(String.class, String.class);
+        Book book1 = bookConstructor.newInstance("红楼梦", "曹雪芹");
+
+        // 方式2.2，用constructor的newinstance，
+        /*Constructor<Book> bookConstructor = Book.class.getConstructor(String.class, String.class);
+        Book book2 = bookConstructor.newInstance("红楼梦", "曹雪芹");
+        book2.toString();*/
+
+        // 方式3
+       /* Book book3 = (Book) book2.clone();
+
+        // 方式4
+        ObjectInputStream objInputStream = new ObjectInputStream(new FileInputStream("data.obj"));
+        Book book4 = (Book) objInputStream.readObject();*/
+    }
+}
